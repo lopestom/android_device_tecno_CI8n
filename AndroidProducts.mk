7f020fc7f267f260916c1e938e6f5ab3ae13f282
lopestorm <guillopes2002@gmail.com>
2023-11-11 14:10:50 -0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X670.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_X670-eng \
+    twrp_X670-userdebug \
+    twrp_X670-user
---
+++ twrp_CI8n.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := TECNO CI8n
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from TECNO-CI8n device
+$(call inherit-product, device/tecno/CI8n/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := CI8n
+PRODUCT_NAME := twrp_CI8n
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO CI8n
+PRODUCT_MANUFACTURER := TECNO
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=TECNO-CI8n \
+    PRODUCT_NAME=CI8n-GL \
+    PRIVATE_BUILD_DESC="vnd_ci8_h816-user 12 SP1A.210812.016 176455 release-keys"
+
+BUILD_FINGERPRINT := TECNO/CI8n-GL/TECNO-CI8n:12/SP1A.210812.016/221202V1668:user/release-keys
